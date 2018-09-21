978417f8a3c3c95d40acc29b8078f95e454462b7
Darren Victoriano <darren.victoriano@gmail.com>
2018-09-20 21:32:34 -0700

    trying to add counter on each label

--- sample.py
+++ sample.py
@@ -2,20 +2,42 @@
 from appJar import gui
 
 
+def samplTest():
+    ui.addLabel("1", "Welcome to appJar1")
+    ui.setLabelBg("1", "#ccc")
+
+
+def samplTest2():
+    ui.addLabel("2", "Welcome to appJar2")
+    ui.setLabelBg("2", "#fff")
+
+
+def samplTest3():
+    ui.addLabel("3", "Welcome to appJar1")
+    ui.setLabelBg("3", "#ccc")
+
+
+def samplTest4():
+    ui.addLabel("4", "Welcome to appJar2")
+    ui.setLabelBg("4", "#fff")
+
+
 # create a GUI variable called app
 ui = gui("Test", "768x480")
+ui.setIcon("img/bot_icon.ico")
 ui.setBg("#B0D1CE")
 
+ui.startFrame("1", row=0, column=0)
+ui.setSticky("NEW")
+ui.setStretch("COLUMN")
+
 # add & configure widgets - widgets get a name, to help referencing them later
-ui.addLabel("title", "Welcome to appJar")
-ui.setLabelBg("title", "#fff")
-
-
-def samplTest():
-    ui.addLabel("2", "Welcome to appJar")
-
-
 ui.after(1000, samplTest)
+ui.after(2000, samplTest2)
+ui.after(3000, samplTest3)
+ui.after(4000, samplTest4)
+ui.stopFrame()
+
 
 # start the GUI
 ui.go()
---
+++ sample2.py
@@ -0,0 +1,54 @@
+# First import the ADB_Action_Script.py it must be on the same folder
+from daaf.ADB_Action_Scipt import ActionScript
+# then import the RC keys and App PKGs for easy scripting
+from daaf.RC_Code import SonyRCKey
+from daaf.AppList import AppList
+from appJar import gui
+
+# create an instance of the class, variables can be change
+tv = ActionScript()
+rc = SonyRCKey()
+app = AppList()
+ui = gui("Sample2", "768x480")
+
+# Format UI
+ui.setIcon("img/bot_icon.ico")
+ui.setBg("#B0D1CE")
+ui.setSticky("NEW")
+ui.setStretch("COLUMN")
+counter = 10
+
+
+def countdown():
+    global counter
+    if counter > 0:
+        ui.setLabel("count", str(counter))
+        counter -= 1
+        ui.after(1000)
+
+
+def f2():
+    global counter = 10
+    ui.addLabel("2", "Do testcase 2", row=1, column=0)
+    ui.addLabel("count", row=0, column=1)
+    ui.setLabelBg("2", "#ccc")
+
+
+def f3():
+    ui.addLabel("2", "Do testcase 2", row=3, column=0)
+    ui.setLabelBg("2", "#ccc")
+
+
+# Automation Start
+# ------------------------------- HDMI1 ----------------------------------
+global counter = 20
+ui.addLabel("1", "Do testcase 1", row=0, column=0)
+ui.addLabel("count", row=0, column=1)
+ui.setLabelBg("1", "#fff")
+
+ui.after(21000, f2)
+
+
+# ------------------------ Launch GUI -----------------------------
+ui.registerEvent(countdown)
+ui.go()
