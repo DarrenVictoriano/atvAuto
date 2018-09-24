c65a8ef4ed548f104a8051618d39c758a003537d
Darren Victoriano <darren.victoriano@gmail.com>
2018-09-24 11:21:06 -0700

    added loop inside loop like damn!

--- sample_tkinter.py
+++ sample_tkinter.py
@@ -2,19 +2,38 @@ from tkinter import *
 from tkinter import ttk
 import time
 
-root = Tk()
-
-count = 0
+count = 1
 
 
 def counter(c=count):
     # global count
     while(c < 10):
-        Label(root, text=c).pack()
+        ttk.Label(app, text=f'Test case {c}').grid(column=2)
         root.update()  # allow window to catch up
         time.sleep(2)
         c += 1
 
 
-counter()
+def repeatIt():
+    for i in range(0, 5):
+        counter()
+        reset()
+        root.update()
+        time.sleep(1)
+        print(i)
+
+
+def reset():
+    for child in app.winfo_children():
+        child.destroy()
+
+
+root = Tk()
+root.title("Loop Tests")
+app = ttk.Frame(root)
+app.widget.config()
+app.grid()
+
+
+repeatIt()
 root.mainloop()
